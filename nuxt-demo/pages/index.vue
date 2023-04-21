93fb38e9cc90b927f1bb8ce4c08ceaaf8fbb925a
zrtch <1160639597@qq.com>
2023-04-21 09:41:43 +0800

    update

---
+++ nuxt-demo/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <img
+      src="https://raw.githubusercontent.com/zrtch/blog-img/master/20230302105021.png"
+      alt=""
+    />
+  </div>
+</template>
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+}
+img {
+  width: 80%;
+  height: 80%;
+}
+</style>
